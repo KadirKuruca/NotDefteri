31b3f36ad84adaaa0c9f9adec1fd581f7ff650a7
Kadir Kuruca <kadirkuruca572@gmail.com>
2018-05-08 22:44:43 +0300

    Veritabanı için Contract Sınıfı oluşturuldu.

---
+++ app/src/main/java/com/example/kadir/notdefteriuygulamasi/data/NotDefteriContract.java
@@ -0,0 +1,27 @@
+package com.example.kadir.notdefteriuygulamasi.data;
+
+import android.provider.BaseColumns;
+
+public class NotDefteriContract {
+
+    public static final class NotlarEntry implements BaseColumns{
+
+        public static final String TABLE_NAME = "notlar";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NOT = "not";
+        public static final String COLUMN_OLUSTURMA_TARİHİ = "olusturulmaTarihi";
+        public static final String COLUMN_BITIS_TARIHI = "bitisTarihi";
+        public static final String COLUMN_YAPILDI = "yapildi";
+        public static final String COLUMN_KATEGORI_ID = "kategoriId";
+
+    }
+
+    public static final class KategoriEntry implements BaseColumns{
+
+        public static final String TABLE_NAME = "kategoriler";
+        
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_KATEGORI = "kategori";
+    }
+}
